c506fd7a1fc59a0e780dbe7f201459b4778653b9
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-09 11:48:58 +0530

    [MODIFIED]:shubham Showing the Player Position

--- src/com/shubham/snakeandladder/SnackAndLadderGame.java
+++ src/com/shubham/snakeandladder/SnackAndLadderGame.java
@@ -2,9 +2,18 @@ package com.shubham.snakeandladder;
 
 public class SnackAndLadderGame {
 
+	private int playerPosition = 0;
+	
+	public void ShowPosition() {
+		
+		System.out.println("Player position = "+playerPosition);
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Snake and Ladder Game ");
+		
+		SnackAndLadderGame snackandladdergame1 = new SnackAndLadderGame();
+		 snackandladdergame1. ShowPosition();
 	}
 
 }
